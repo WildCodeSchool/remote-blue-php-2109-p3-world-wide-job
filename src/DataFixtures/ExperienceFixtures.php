2f6077920f78982420f8b7cd4d9c304bbaf2e15b
Naim Jhuboo <naim.jhuboo@gmail.com>
2021-12-16 16:51:42 +0100

    entity

--- src/DataFixtures/ExperienceFixtures.php
+++ src/DataFixtures/ExperienceFixtures.php
@@ -2,32 +2,31 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Training;
+use App\Entity\Experience;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TrainingFixtures extends Fixture implements DependentFixtureInterface
+class ExperienceFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $maxCurriculum = 5;
         for ($i = 0; $i < $maxCurriculum; $i++) {
-            $maxTraining = 2;
-            for ($itterator = 0; $itterator <= $maxTraining; $itterator++) {
-                $training = new Training();
-                $training->setCurriculum($this->getReference('curriculum_' . $i))
-                    ->setFieldOfStudy('Economie')
-                    ->setSchool('Ecole de commerce de Nantes')
-                    ->setDegree('Master')
-                    ->setGraduate(true)
+            $maxExperience = 2;
+            for ($itterator = 0; $itterator <= $maxExperience; $itterator++) {
+                $experience = new Experience();
+                $experience->setCurriculum($this->getReference('curriculum_' . $i))
+                    ->setCompany('Airbus')
+                    ->setLocalisation('Toulouse')
+                    ->setContractType(rand(1, 3))
                     ->setDateIn(new DateTime('20016-10-12'))
                     ->setDateOut(new DateTime('2019-10-12'))
                     ->setDescription('Aut dolorem repudiandae ut odio ut sint officia eos officia quia est 
                     provident minima quo dolores eius. Et quam modi aut eaque saepe est aliquid cumque a libero 
                     molestias non minus veniam.');
-                $manager->persist($training);
+                $manager->persist($experience);
             }
         }
         $manager->flush();
