db1e9760fe282e2777b88205584490a8c146dfae
Pierre Bailly <pierre.bailly23@sfr.fr>
2021-12-20 06:52:57 -0800

    US-7.3 page de recherche mise en page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('rechercheBase.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
