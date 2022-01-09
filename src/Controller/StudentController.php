29549133919f6817a2425ef47d19e07849fb53c8
Naim Jhuboo <naim.jhuboo@gmail.com>
2022-01-09 01:08:58 +0100

    student profile

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/etudiant", name="student_")
+ */
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/{}", name="student")
+     */
+    /**
+     * @Route("/{slug}", name="show")
+     */
+    public function show(Student $student): Response
+    {
+        return $this->render('student/show.html.twig', [
+            'student' => $student,
+        ]);
+    }
+}
