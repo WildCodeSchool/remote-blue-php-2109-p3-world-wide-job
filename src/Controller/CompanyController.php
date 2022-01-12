2d6477ff6b74320c49ee9ef2626ad9547c1fa7fb
SamuelGuilloton <samuel.guilloton01@gmail.com>
2022-01-12 09:29:30 +0100

    fix

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Company;
 use App\Entity\Offer;
+use App\Form\Company1Type;
+use App\Form\CompanyEditType;
+use App\Form\CompanyType;
+use App\Form\PasswordEditType;
 use App\Repository\OfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +21,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CompanyController extends AbstractController
 {
     /**
-     * @Route("/{slug}", name="show")
+     * @Route("/{id}", name="show")
      */
     public function show(Company $company, OfferRepository $offerRepository): Response
     {
@@ -26,5 +32,49 @@ class CompanyController extends AbstractController
             'company' => $company,
             'applications' => $applications
         ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Company $company, EntityManagerInterface $entityManager): Response
+    {
+        $companyForm = $this->createForm(CompanyType::class, $company);
+        $companyForm->handleRequest($request);
+
+        $passwordForm = $this->createForm(PasswordEditType::class, $company);
+        $passwordForm->handleRequest($request);
+
+        if ($companyForm->isSubmitted() && $companyForm->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company_show', ['id' => $company->getId() ], Response::HTTP_SEE_OTHER);
+        }
+
+        if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company_show', ['id' => $company->getId() ], Response::HTTP_SEE_OTHER);
+        }
+
+
+        return $this->renderForm('company/edit.html.twig', [
+            'company' => $company,
+            'form' => $companyForm,
+            'passwordForm' => $passwordForm
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="delete", methods={"POST"})
+     */
+    public function delete(Request $request, Company $company, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $company->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($company);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('company_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/UserEditType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('adress1', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('adress2', TextType::class, [
+                'label' => 'Adresse (suite)',
+                'required' => false
+            ])
+            ->add('zip', TextType::class, [
+                'label' => 'Code Postal'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Mail'
+            ])
+
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
