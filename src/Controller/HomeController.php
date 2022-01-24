77f216277e62189babfd95906e3318e27225ff40
Naim Jhuboo <naim.jhuboo@gmail.com>
2022-01-24 10:11:34 +0100

    stat homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ApplicationRepository;
+use App\Repository\CompanyRepository;
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,20 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        StudentRepository $studentRepository,
+        ApplicationRepository $applicationRepository,
+        CompanyRepository $companyRepository
+    ): Response {
+        $countStudents = $studentRepository->findAll();
+        $countApplications = $applicationRepository->findAll();
+        $countCompanies = $companyRepository->findAll();
+        $signedApllications = $applicationRepository->findBy(['status' => 3]);
+        return $this->render('home/index.html.twig', [
+            'countStudents' => $countStudents,
+            'countApplications' => $countApplications,
+            'countCompanies' => $countCompanies,
+            'signedApplications' =>$signedApllications
+        ]);
     }
 }
