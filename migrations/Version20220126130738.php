42a25344f88cd15df1ae3a2542634cfa97a020aa
Naim Jhuboo <naim.jhuboo@gmail.com>
2022-01-26 14:08:17 +0100

    us211

---
+++ migrations/Version20220126130738.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220126130738 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE student_offer (student_id INT NOT NULL, offer_id INT NOT NULL, INDEX IDX_A1E1FEA3CB944F1A (student_id), INDEX IDX_A1E1FEA353C674EE (offer_id), PRIMARY KEY(student_id, offer_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE student_offer ADD CONSTRAINT FK_A1E1FEA3CB944F1A FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE student_offer ADD CONSTRAINT FK_A1E1FEA353C674EE FOREIGN KEY (offer_id) REFERENCES offer (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE offer ADD status TINYINT(1) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE student_offer');
+        $this->addSql('ALTER TABLE offer DROP status');
+    }
+}
