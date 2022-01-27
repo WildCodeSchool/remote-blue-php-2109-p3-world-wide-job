6d0369f3b3af44fb845323621559b3d6cd1844d5
Samuel Guilloton <90616927+SamuelGUILLOTON@users.noreply.github.com>
2022-01-27 13:54:33 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -86,8 +86,6 @@ class RegistrationFormType extends AbstractType
         ;
     }
 
-
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
