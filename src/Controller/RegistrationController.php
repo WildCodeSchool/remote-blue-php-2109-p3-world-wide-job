51a063ffdb47fdfab787acb56ae7753cd02de9eb
SamuelGuilloton <samuel.guilloton01@gmail.com>
2022-01-31 10:16:42 +0100

    titre dynamique formulaire

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -226,6 +226,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'role' => $role
         ]);
     }
 
